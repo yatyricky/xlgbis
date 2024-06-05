6b36dd413c2b0c9f83075fc443c0be9dfdda6f0d
Rick Sun <yatyricky@gmail.com>
2024-06-05 18:22:08 +0800

    1

--- xlgbis-client/src/User/AccDocQuery.jsx
+++ xlgbis-client/src/User/AccDocQuery.jsx
@@ -2,22 +2,46 @@ import React, { useCallback, useEffect, useLayoutEffect, useState } from "react"
 import HttpTask from "../HttpTask";
 import { Container, Spinner } from "react-bootstrap";
 import Board from "../Board";
-import List from 'rc-virtual-list';
+import VirtualTable from "rc-table/lib/VirtualTable"
+
+let data = []
+for (let i = 0; i < 10000; i++) {
+    data[i] = {
+        id: i,
+        name: Array(Math.floor(Math.random() * 5) + 3).fill(1).map(e => String.fromCharCode(Math.floor(Math.random() * 26) + 65)).join(""),
+        age: Math.floor(Math.random() * 26) + 5,
+    }
+}
+
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+        width: 50,
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        width: 100,
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+        width: 75,
+    },
+    {
+        title: 'Editable',
+        dataIndex: '',
+        key: 'editable',
+        render: () => <input></input>,
+    },
+];
 
 export default () => {
-    let data = []
-    for (let i = 0; i < 10000; i++) {
-        data[i] = {
-            id: i,
-            name: Array(Math.floor(Math.random() * 5) + 3).map(e => Math.floor(Math.random() * 26) + 65).join("")
-        }
-    }
-    console.log(data[0].name);
     return (
-        <>
-            <List style={{ border: "1px solid black" }} data={data} height={200} itemHeight={30} itemKey={(e) => e.id}>
-                {e => <div>{e.name}</div>}
-            </List>
-        </>
+        <VirtualTable scroll={{ y: 200 }} height={600} data={data} columns={columns} itemKey={(e) => e.id} />
     )
 }
